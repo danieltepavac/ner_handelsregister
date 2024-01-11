877f333328c23415a245f739621f9254653ebef6
danieltepavac <daniel.tepavac@gmail.com>
2024-01-11 11:20:39 +0100

    fix: move old script into other directory

--- app/prepare_annotation.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import json 
-
-path = Path(Path("__file__").parent, "../data/new_data_to_annotate.jsonl")
-
-with open(path, "r") as f: 
-    data = json.load(f)
-
-print(len(data))
