e8208fb9b3d0b025037c6986b7ac23f8fa002e0e
danieltepavac <daniel.tepavac@gmail.com>
2024-01-20 11:41:26 +0100

    feat: new organisation of experiments

---
+++ app/experiment1_detailed_annotation/spacy_evaluate_2_with_eval.py
@@ -0,0 +1,62 @@
+# Import necessary libraries.
+import json
+
+from pathlib import Path
+
+import spacy
+from spacy.training.example import Example
+from spacy.scorer import Scorer
+
+
+# Save directory model is saved at. 
+model_directory = Path(Path(__file__).parents[1], "../models/experiment1_detailed_annotation/ner2_with_eval")
+
+# Load the trained model from the output directory.
+nlp = spacy.load(model_directory)
+
+# Path to evaluation data. 
+test_path = Path(Path(__file__).parents[1], "../data/experiment1_detailed_annotation/test_data.json")
+
+# Open EVALUATION_DATA.
+with open(test_path, "r") as f:
+    TEST_DATA = json.load(f)
+
+# Create empty list of examples. 
+examples = []
+
+# Iterate over EVALUATION_DATA_TUPLES.
+for text, annotations in TEST_DATA: 
+    # Apply custom model on every text. 
+    doc = nlp(text)
+    # Save predictions and gold standard in example. 
+    example = Example.from_dict(doc, annotations)
+    # Save individual example in list of examples. 
+    examples.append(example)
+
+# Save the scorer. 
+scorer = Scorer()
+
+# Apply the scorer on list of examples. 
+scores = scorer.score(examples=examples)
+
+# Print evaluation metrics. 
+print("Precision:", scores["ents_p"])
+print("Recall:", scores["ents_r"])
+print("F1 score:", scores["ents_f"])
+
+# Save results in Json. 
+save_path = Path(Path(__file__).parents[1], "../results/experiment1_detailed_annotation/2_with_eval.json")
+
+results = {
+    'Precision': scores["ents_p"],
+    'Recall': scores["ents_r"],
+    'F1 score': scores["ents_f"]
+}
+
+with open(save_path, "w", encoding="utf-8") as f: 
+    json.dump(results, f, indent=2, ensure_ascii=False)
+
+
+
+
+
---
+++ app/experiment1_detailed_annotation/spacy_test_0.py
@@ -0,0 +1,25 @@
+# Import spacy and displacy.
+import spacy
+from spacy import displacy 
+
+# First try: Load German LM. 
+NER = spacy.load("de_core_news_lg")
+
+
+# This is the example text.
+example = "Wir sind die alleinigen Gesellschafter der im Handelsregister des Amtsgerichts Köln unter HR B 81000 eingetragenen mit Sitz in Köln. Gesellschafterbeschluss First Contor Immobilien GmbH Unter Verzicht auf alle gesetz- und satzungsmäßigen Frist- und Formvorschriften halten wir hiermit eine Gesellschafterversammlung ab und beschließen einstimmig wie folgt: 등 (Dominik Recke) Herr Ismail C in a r, geboren am 01. November 1964, wohnhaft Scharpenburger Straße 24 A, 58256 Ennepetal, wird zum weiteren Geschäftsführer bestellt. Er ist einzelvertretungsbefugt und von den Beschränkungen des § 181 BGB befreit. Weiteres war nicht zu beschließen. Köln, den 08. Juni 2020 سکا اردو کے (Ismail Cinar)"
+
+# Apply NER on example.
+NER_example = NER(example)
+
+# Show each recognized entity + its label.
+for word in NER_example.ents:
+    print(word.text, word.label_)
+
+# Show how the text is marked up. In Browser: 
+displacy.serve(NER_example, style="ent")
+
+# Show how the text is marked up. In Notebook:
+displacy.render(NER_example, style="ent")  
+
+# Result: It does work but not every recognized entity is an entity.
